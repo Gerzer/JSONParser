78974cf9ec171d15f10b1e4925d41cb574f63af5
Gabriel Jacoby-Cooper <gerzersoftware@icloud.com>
2020-11-28 16:42:32 -0500

    Fix compatibility metadata

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 let package = Package(
 	name: "JSONParser",
 	platforms: [
-		.macOS(.v10_15)
+		.macOS(.v10_15),
+		.iOS(.v13),
+		.watchOS(.v6),
+		.tvOS(.v13)
 	],
 	products: [
 		.library(
